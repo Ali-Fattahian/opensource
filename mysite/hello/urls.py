d08f3f7931d513f8ae556aa3c7ef394f28525633
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-05-22 17:38:06 +0430

    create end points for CRUD operations

--- mysite/hello/urls.py
+++ mysite/hello/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('posts', views.BlogPostListCreate.as_view(), name='blog-post-list-create'),
+    path('posts/<slug:slug>', views.BlogPostDetail.as_view(), name='blog-post-detail'),
 ]
--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.generics import (
+    RetrieveUpdateDestroyAPIView,
+    ListCreateAPIView
+)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import BlogPost
+from .serializers import BlogPostSerializer
 
 
 def index(request):
     return HttpResponse("Hello, world!")
 
+
+class BlogPostDetail(RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    lookup_field = 'slug'
+
+
+class BlogPostListCreate(ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
