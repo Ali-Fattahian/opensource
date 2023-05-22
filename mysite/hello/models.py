5377d65c563e0f53ede8a48963f4d08ddf4eb012
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-05-22 17:21:45 +0430

    create blogpost model, run migrations

--- mysite/hello/models.py
+++ mysite/hello/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Title: {self.title}, Author: {self.author.username}'
